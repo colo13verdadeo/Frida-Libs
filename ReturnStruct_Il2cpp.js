51b0354a94e8bf74ce39d0a006998741bff4d15d
colo13 <joa_028@hotmail.com>
2024-04-08 22:38:04 +0200

    Create ReturnStruct_Il2cpp.js

---
+++ ReturnStruct_Il2cpp.js
@@ -0,0 +1,7 @@
+//Il2cpp
+//Para devolver una estructura especifica en Il2cpp se debe usar
+        return (0) //Si no funciona usar el siguiente
+  retvalue.method("Set").overload("System.Int32").invoke(0);
+  return (retvalue);
+//Set es la funcion para construir la estructura.
+//Overload(TIPO)
